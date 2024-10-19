571210b74e039fec87769791ddd600d02e8abb00
myoungjinGo-FE <cokemania2099@gmail.com>
2024-10-20 00:11:58 +0900

    🎨 Fix: Image를 URL을 받도록 변경

--- product/models.py
+++ product/models.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    thumbnail = models.ImageField(upload_to='product_thumbnails/')
+    thumbnail = models.URLField()
     description_images = models.ManyToManyField('ProductImage', related_name='products')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_rate = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal('0.00'))
@@ -21,17 +21,17 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-    image = models.ImageField(upload_to='product_description_images/')
+    image = models.URLField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Image {self.id} for product"
 
+
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='subcategories')
     name = models.CharField(max_length=255, unique=True)
-
 
     def __str__(self):
         return self.name
@@ -40,11 +40,11 @@ class Category(models.Model):
 class ProductCategory(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['product', 'category'], name='unique_product_category')
         ]
-
 
     def __str__(self):
         return f"{self.product.name} in {self.category.name}"
