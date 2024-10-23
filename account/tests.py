1862e6405b27b9b3022f67b008037e1aaabd7c73
sungwook <ssohn3@buffalolo.edu>
2024-10-23 17:21:13 +0900

    ⚡️ feat: account/tests.py 응답 데이터 검증 추가 및 테스트 리팩토링

--- account/tests.py
+++ account/tests.py
@@ -1,27 +1,16 @@
 import pytest
-import requests
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import AccessToken
 from django.urls import reverse
 from account.models import Cat, CatBreed, User
 
-from account.models import User
-from forcatProject.settings import KAKAO_CLIENT_ID
 
 @pytest.fixture(scope="session")
 def api_client():
     return APIClient()
 
-class TestCase:
-    def test_JWT_토큰_발급_테스트(self):
-        user = User(username="username", nickname="nickname")
-        access_token = AccessToken.for_user(user)
-        assert access_token
-
-
 @pytest.fixture
 def user(db):
-    # Custom User 모델에 맞춘 유저 생성
     return User.objects.create(
         username='testuser',
         nickname='testnickname',
@@ -34,77 +23,123 @@ def user(db):
         google_id='google123'
     )
 
-
 @pytest.fixture
 def cat_breed(db):
-    # 고양이 묘종 생성
     return CatBreed.objects.create(category_id=1, breed_type='Persian', rank=1)
 
-
 @pytest.fixture
-def cat(db, user, cat_breed):
-    # 고양이 데이터 생성
-    return Cat.objects.create(
-        name='Kitty',
-        cat_breed=cat_breed,
-        birth_date='2020-01-01',
-        gender=1,
-        is_neutered=1,
-        weight=4.50,
-        user=user
-    )
+def create_cats(db, user, cat_breed):
+    def _create_multiple_cats():
+        return [
+            Cat.objects.create(
+                name='Kitty1',
+                cat_breed=cat_breed,
+                birth_date='2020-01-01',
+                gender=1,
+                is_neutered=1,
+                weight=4.50,
+                user=user
+            ),
+            Cat.objects.create(
+                name='Kitty2',
+                cat_breed=cat_breed,
+                birth_date='2019-06-15',
+                gender=0,
+                is_neutered=0,
+                weight=5.00,
+                user=user
+            ),
+            Cat.objects.create(
+                name='Kitty3',
+                cat_breed=cat_breed,
+                birth_date='2018-08-25',
+                gender=1,
+                is_neutered=1,
+                weight=3.80,
+                user=user
+            ),
+        ]
+    return _create_multiple_cats
 
 
-def test_create_cat(api_client, user, cat_breed):
-    # 고양이 생성 테스트
-    api_client.force_authenticate(user=user)
-    url = reverse('cat-list')  # router에 등록된 'cats' URL reverse
-    data = {
-        'name': 'NewCat',
-        'cat_breed': cat_breed.category_id,
-        'birth_date': '2021-05-20',
-        'gender': 0,
-        'is_neutered': 1,
-        'weight': 3.20,
-        'user': user.id
-    }
-    response = api_client.post(url, data, format='json')
-    assert response.status_code == 201
-    assert response.data['name'] == 'NewCat'
+class TestJWTToken:
+    def test_JWT_토큰_발급_테스트(self):
+        user = User(username="username", nickname="nickname")
+        access_token = AccessToken.for_user(user)
+        assert access_token
 
 
-def test_get_cat(api_client, cat):
-    # 고양이 목록 조회 테스트
-    url = reverse('cat-list')
-    response = api_client.get(url)
-    assert response.status_code == 200
-    assert len(response.data) == 1
-    assert response.data[0]['name'] == cat.name
+class TestCatCRUD:
+    def test_고양이_생성_테스트(self, api_client, user, cat_breed):
+        api_client.force_authenticate(user=user)
+        url = reverse('cat-list')
+        data = {
+            'name': 'NewCat',
+            'cat_breed': cat_breed.category_id,
+            'birth_date': '2021-05-20',
+            'gender': 0,
+            'is_neutered': 1,
+            'weight': 3.20,
+            'user': user.id
+        }
+        response = api_client.post(url, data, format='json')
 
+        assert response.status_code == 201
 
-def test_update_cat(api_client, user, cat):
-    # 고양이 업데이트 테스트
-    api_client.force_authenticate(user=user)
-    url = reverse('cat-detail', args=[cat.cat_id])
-    data = {
-        'name': 'UpdatedCat',
-        'cat_breed': cat.cat_breed.category_id,
-        'birth_date': '2020-01-01',
-        'gender': 1,
-        'is_neutered': 1,
-        'weight': 5.00,
-        'user': user.id
-    }
-    response = api_client.put(url, data, format='json')
-    assert response.status_code == 200
-    assert response.data['name'] == 'UpdatedCat'
-    assert response.data['weight'] == '5.00'
+        assert response.json() == {
+            "name": "NewCat",
+            "cat_breed": cat_breed.category_id,
+            "birth_date": "2021-05-20",
+            "gender": 0,
+            "is_neutered": 1,
+            "weight": "3.20",
+            "user": user.id,
+        }
 
+    def test_고양이_목록_조회_테스트(self, api_client, create_cats):
+        cats = create_cats()
 
-def test_delete_cat(api_client, user, cat):
-    # 고양이 삭제 테스트
-    api_client.force_authenticate(user=user)
-    url = reverse('cat-detail', args=[cat.cat_id])
-    response = api_client.delete(url)
-    assert response.status_code == 204
-    assert Cat.objects.count() == 0
+        url = reverse('cat-list')
+        response = api_client.get(url)
+        assert response.status_code == 200
+        assert len(response.data) == 3
+
+        assert response.data[0]['name'] == cats[0].name
+        assert response.data[1]['name'] == cats[1].name
+        assert response.data[2]['name'] == cats[2].name
+
+    def test_고양이_업데이트_테스트(self, api_client, user, create_cats):
+        api_client.force_authenticate(user=user)
+        cats = create_cats()
+        cat_to_update = cats[0]
+        url = reverse('cat-detail', args=[cat_to_update.cat_id])
+        data = {
+            'name': 'UpdatedCat',
+            'cat_breed': cat_to_update.cat_breed.category_id,
+            'birth_date': '2020-01-01',
+            'gender': 1,
+            'is_neutered': 1,
+            'weight': 5.00,
+            'user': user.id
+        }
+        response = api_client.put(url, data, format='json')
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "name": "UpdatedCat",
+            "cat_breed": cat_to_update.cat_breed.category_id,
+            "birth_date": "2020-01-01",
+            "gender": 1,
+            "is_neutered": 1,
+            "weight": "5.00",
+            "user": user.id,
+        }
+
+    def test_고양이_삭제_테스트(self, api_client, user, create_cats):
+        api_client.force_authenticate(user=user)
+        cats = create_cats()
+        cat_to_delete = cats[0]
+        url = reverse('cat-detail', args=[cat_to_delete.cat_id])
+        response = api_client.delete(url)
+        assert response.status_code == 204
+        assert Cat.objects.count() == 2
