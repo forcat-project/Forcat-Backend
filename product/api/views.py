e59bca1016314bad191fa5bfe0e69e266326f654
myoungjinGo-FE <cokemania2099@gmail.com>
2024-10-24 09:31:14 +0900

    ✨ Feat: 상품 장바구니 추가

--- product/api/views.py
+++ product/api/views.py
@@ -4,13 +4,17 @@ from rest_framework.pagination import CursorPagination
 from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
 
+from account.models import User
 from product.api.filter import ProductFilter
 from product.api.serializer import (
     ProductSerializer,
     CategoryListSerializer,
     CategorySerializer,
+    CartItemReadSerializer,
+    CartItemCreateSerializer,
+    CartItemSerializer,
 )
-from product.models import Product, Category
+from product.models import Product, Category, CartItem, Cart
 
 
 class MyCursorPagination(CursorPagination):
@@ -51,3 +55,61 @@ class CategoryViewSet(
         instance = self.get_object()
         serializer = CategorySerializer(instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        return CartItem.objects.filter(cart__user__id=user_id).select_related("product")
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return CartItemCreateSerializer
+        elif self.action in ["partial_update", "delete"]:
+            return CartItemSerializer
+        return CartItemReadSerializer
+
+    def create(self, request, *args, **kwargs):
+        user_id = self.kwargs.get("user_id")
+        serializer = self.get_serializer(
+            data=request.data, context={"user_id": user_id}
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        user_id = self.kwargs.get("user_id")
+        product_id = self.kwargs.get("products_id")
+
+        cart = Cart.objects.get(user_id=user_id)
+        cart_item = CartItem.objects.get(cart_id=cart.id, product_id=product_id)
+
+        serializer = self.get_serializer(
+            instance=cart_item,
+            data={
+                "quantity": request.data["quantity"],
+            },
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        user_id = self.kwargs.get("user_id")
+        product_id = self.kwargs.get("products_id")
+
+        # 해당 user_id의 장바구니에서 product_id에 해당하는 상품을 찾음
+        try:
+            cart = Cart.objects.get(user_id=user_id)
+            cart_item = CartItem.objects.get(cart_id=cart.id, product_id=product_id)
+            cart_item.delete()  # 장바구니 아이템 삭제
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except CartItem.DoesNotExist:
+            return Response(
+                {"error": "해당 상품이 장바구니에 없습니다."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
