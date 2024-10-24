356592528421d38d764f1a364bfb94910a84bf16
sungwook <ssohn3@buffalolo.edu>
2024-10-24 11:48:39 +0900

    ⚡️ feat: 할인율 내림차순 정렬 테스트를 response.jsion() 사용하여 응답을 직접 출력하는 방식으로 테스트 하게 수정했습니다.

--- product/tests.py
+++ product/tests.py
@@ -9,6 +9,7 @@ from product.models import Product, Category, ProductCategory
 def api_client():
     return APIClient()
 
+
 @pytest.fixture()
 def 테스트_상품_생성():
     product = Product.objects.create(
@@ -27,6 +28,7 @@ def 테스트_상품_생성():
 
     ProductCategory.objects.create(product=product, category=category_1)
     ProductCategory.objects.create(product=product, category=category_2)
+
 
 @pytest.fixture()
 def 테스트_여러_상품_생성():
@@ -51,6 +53,7 @@ def 테스트_여러_상품_생성():
             ProductCategory.objects.create(product=product, category=category_2)
         elif i % 3 == 0:
             ProductCategory.objects.create(product=product, category=category_3)
+
 
 @pytest.fixture()
 def 테스트_한글_이름_여러_상품_생성():
@@ -79,6 +82,7 @@ def 테스트_한글_이름_여러_상품_생성():
         else:
             ProductCategory.objects.create(product=product, category=category_2)
 
+
 @pytest.fixture()
 def 테스트_대량_상품_생성():
     category_1 = Category.objects.create(name="category_1")
@@ -103,10 +107,13 @@ def 테스트_대량_상품_생성():
         elif i % 3 == 0:
             ProductCategory.objects.create(product=product, category=category_3)
 
+
 @pytest.fixture()
 def 테스트_67카테고리_생성():
     category_67 = Category.objects.create(category_id=67, name="카테고리_67")
-    category_68 = Category.objects.create(name="카테고리_68", parent_category=category_67)
+    category_68 = Category.objects.create(
+        name="카테고리_68", parent_category=category_67
+    )
     return category_67, category_68
 
 
@@ -189,7 +196,9 @@ class TestProductCRUD:
             ],
         }
 
-    def test_모든_상품_조회_카테고리_필터링_테스트(self, api_client, 테스트_여러_상품_생성):
+    def test_모든_상품_조회_카테고리_필터링_테스트(
+        self, api_client, 테스트_여러_상품_생성
+    ):
         url = reverse("product-list")
         response = api_client.get(url + "?categories=2&categories=3")
         assert response.status_code == 200
@@ -232,7 +241,9 @@ class TestProductCRUD:
             ],
         }
 
-    def test_모든_상품_조회_이름_필터링_테스트(self, api_client, 테스트_한글_이름_여러_상품_생성):
+    def test_모든_상품_조회_이름_필터링_테스트(
+        self, api_client, 테스트_한글_이름_여러_상품_생성
+    ):
         url = reverse("product-list")
         response = api_client.get(url + "?name=맛있다")
         assert response.status_code == 200
@@ -272,6 +283,7 @@ class TestProductCRUD:
     def test_상품_카테고리_조회_테스트(self, api_client, 테스트_여러_상품_생성):
         url = reverse("category-list")
         response = api_client.get(url)
+
         assert response.status_code == 200
         assert response.json() == [
             {
@@ -288,14 +300,15 @@ class TestProductCRUD:
     def test_특정_카테고리_조회_테스트(self, api_client, 테스트_67카테고리_생성):
         url = reverse("category-list")
         response = api_client.get(url)
+
         assert response.status_code == 200
-        data = response.json()
-        assert len(data) == 1
-        assert data[0]["category_id"] == 67
-        assert data[0]["name"] == "카테고리_67"
-        assert len(data[0]["subcategories"]) == 1
-        assert data[0]["subcategories"][0]["category_id"] == 68
-        assert data[0]["subcategories"][0]["name"] == "카테고리_68"
+        assert response.json() == [
+            {
+                "category_id": 67,
+                "name": "카테고리_67",
+                "subcategories": [{"category_id": 68, "name": "카테고리_68"}],
+            }
+        ]
 
 
 @pytest.mark.django_db
@@ -304,19 +317,115 @@ class TestProductOrdering:
         url = "/api/products?ordering=-discount_rate"
         response = api_client.get(url)
         assert response.status_code == 200
-        results = response.json().get("results", [])
-        assert results[0]["discount_rate"] == "15.00"
-        assert results[1]["discount_rate"] == "10.00"
-        assert results[2]["discount_rate"] == "5.00"
+        assert response.json() == {
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "product_id": 3,
+                    "categories": [
+                        {"category_id": 1, "name": "category_1"},
+                        {"category_id": 3, "name": "category_3"},
+                    ],
+                    "discounted_price": 255.0,
+                    "name": "Test Product 3",
+                    "thumbnail_url": "https://url/test_thumbnail_3.jpg",
+                    "company": "ohYes",
+                    "description_image_url": "https://url/test_image_3.jpg",
+                    "price": "300.00",
+                    "discount_rate": "15.00",
+                    "remain_count": 0,
+                    "purchase_count": 15,
+                },
+                {
+                    "product_id": 2,
+                    "categories": [
+                        {"category_id": 1, "name": "category_1"},
+                        {"category_id": 2, "name": "category_2"},
+                    ],
+                    "discounted_price": 180.0,
+                    "name": "Test Product 2",
+                    "thumbnail_url": "https://url/test_thumbnail_2.jpg",
+                    "company": "ohYes",
+                    "description_image_url": "https://url/test_image_2.jpg",
+                    "price": "200.00",
+                    "discount_rate": "10.00",
+                    "remain_count": 0,
+                    "purchase_count": 10,
+                },
+                {
+                    "product_id": 1,
+                    "categories": [{"category_id": 1, "name": "category_1"}],
+                    "discounted_price": 95.0,
+                    "name": "Test Product 1",
+                    "thumbnail_url": "https://url/test_thumbnail_1.jpg",
+                    "company": "ohYes",
+                    "description_image_url": "https://url/test_image_1.jpg",
+                    "price": "100.00",
+                    "discount_rate": "5.00",
+                    "remain_count": 0,
+                    "purchase_count": 5,
+                },
+            ],
+        }
 
-    def test_상품_구매횟수_내림차순_정렬_테스트(self, api_client, 테스트_여러_상품_생성):
+    def test_상품_구매횟수_내림차순_정렬_테스트(
+        self, api_client, 테스트_여러_상품_생성
+    ):
         url = "/api/products?ordering=-purchase_count"
         response = api_client.get(url)
         assert response.status_code == 200
-        results = response.json().get("results", [])
-        assert results[0]["purchase_count"] == 15
-        assert results[1]["purchase_count"] == 10
-        assert results[2]["purchase_count"] == 5
+        assert response.json() == {
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "product_id": 3,
+                    "categories": [
+                        {"category_id": 1, "name": "category_1"},
+                        {"category_id": 3, "name": "category_3"},
+                    ],
+                    "discounted_price": 255.0,
+                    "name": "Test Product 3",
+                    "thumbnail_url": "https://url/test_thumbnail_3.jpg",
+                    "company": "ohYes",
+                    "description_image_url": "https://url/test_image_3.jpg",
+                    "price": "300.00",
+                    "discount_rate": "15.00",
+                    "remain_count": 0,
+                    "purchase_count": 15,
+                },
+                {
+                    "product_id": 2,
+                    "categories": [
+                        {"category_id": 1, "name": "category_1"},
+                        {"category_id": 2, "name": "category_2"},
+                    ],
+                    "discounted_price": 180.0,
+                    "name": "Test Product 2",
+                    "thumbnail_url": "https://url/test_thumbnail_2.jpg",
+                    "company": "ohYes",
+                    "description_image_url": "https://url/test_image_2.jpg",
+                    "price": "200.00",
+                    "discount_rate": "10.00",
+                    "remain_count": 0,
+                    "purchase_count": 10,
+                },
+                {
+                    "product_id": 1,
+                    "categories": [{"category_id": 1, "name": "category_1"}],
+                    "discounted_price": 95.0,
+                    "name": "Test Product 1",
+                    "thumbnail_url": "https://url/test_thumbnail_1.jpg",
+                    "company": "ohYes",
+                    "description_image_url": "https://url/test_image_1.jpg",
+                    "price": "100.00",
+                    "discount_rate": "5.00",
+                    "remain_count": 0,
+                    "purchase_count": 5,
+                },
+            ],
+        }
 
 
 @pytest.mark.django_db
@@ -357,38 +466,35 @@ class TestDiscountRateFiltering:
         url = "/api/products?ordering=-discount_rate"
         response = api_client.get(url)
 
-        assert response.status_code == 200
-
-        print(response.json())
-
-        results = response.json().get('results', [])
-
-        assert results[0] == {
-            'product_id': product_2.product_id,
-            'name': product_2.name,
-            'company': product_2.company,
-            'thumbnail_url': product_2.thumbnail_url,
-            'description_image_url': product_2.description_image_url,
-            'price': "200.00",
-            'discount_rate': "15.00",
-            'purchase_count': product_2.purchase_count,
-            'categories': [],
-            'discounted_price': 170.0,
-            'remain_count': 0,
+        assert response.json() == {
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "product_id": 2,
+                    "categories": [],
+                    "discounted_price": 170.0,
+                    "name": "Product 2",
+                    "thumbnail_url": "https://url/product2.jpg",
+                    "company": "Company B",
+                    "description_image_url": "https://url/product2_desc.jpg",
+                    "price": "200.00",
+                    "discount_rate": "15.00",
+                    "remain_count": 0,
+                    "purchase_count": 5,
+                },
+                {
+                    "product_id": 3,
+                    "categories": [],
+                    "discounted_price": 135.0,
+                    "name": "Product 3",
+                    "thumbnail_url": "https://url/product3.jpg",
+                    "company": "Company C",
+                    "description_image_url": "https://url/product3_desc.jpg",
+                    "price": "150.00",
+                    "discount_rate": "10.00",
+                    "remain_count": 0,
+                    "purchase_count": 8,
+                },
+            ],
         }
-        assert results[1] == {
-            'product_id': product_3.product_id,
-            'name': product_3.name,
-            'company': product_3.company,
-            'thumbnail_url': product_3.thumbnail_url,
-            'description_image_url': product_3.description_image_url,
-            'price': "150.00",
-            'discount_rate': "10.00",
-            'purchase_count': product_3.purchase_count,
-            'categories': [],
-            'discounted_price': 135.0,
-            'remain_count': 0,
-        }
-
-
-
