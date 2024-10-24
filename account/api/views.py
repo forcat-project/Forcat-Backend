2d30555dfd1c4fa1b8133ec812a189f2531428f4
myoungjinGo-FE <cokemania2099@gmail.com>
2024-10-24 14:30:31 +0900

    ✨ Feat: 유저 조회 및 수정 추가

--- account/api/views.py
+++ account/api/views.py
@@ -1,18 +1,30 @@
 from account.models import Cat
 from account.api.serializers import CatSerializer
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
 from account.api.serializer import UserSerializer, FileUploadSerializer
+from account.api.serializer import UserSerializer, UserUpdateSerializer
 from account.models import User
 
 
-class UserViewSet(viewsets.GenericViewSet):
+class UserViewSet(
+    viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin
+):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    default_serializer_class = UserSerializer  # 기본 직렬화기
+    update_serializer_class = UserUpdateSerializer  # 업데이트에 사용할 직렬화기
+
+    def get_serializer_class(self):
+        # 'update', 'partial_update' 요청일 때 다른 serializer 사용
+        if self.action in ["update", "partial_update"]:
+            return self.update_serializer_class
+        return self.default_serializer_class
 
     @action(methods=["POST"], detail=False, url_path="sign-up")
     def sign_up(self, request):
