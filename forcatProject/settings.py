b8c4608987e7eeef8a0f6938b32e36683c4fae2f
myoungjinGo-FE <cokemania2099@gmail.com>
2024-10-29 15:45:59 +0900

    🔧 Fix: docker 환경 분기처리

--- forcatProject/settings.py
+++ forcatProject/settings.py
@@ -164,7 +164,7 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",  # 'redis_boot' 대신 'redis'로 설정
+        "LOCATION": "redis://127.0.0.1:6379/1",  # 'redis_boot' 대신 'redis'로 설정
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
