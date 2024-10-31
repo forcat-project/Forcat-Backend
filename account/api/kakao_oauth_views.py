23b38fcae4dd715b49192bc559c5f9a48665e2d7
myoungjinGo-FE <cokemania2099@gmail.com>
2024-10-31 16:30:39 +0900

    🎨 Fix: 토큰을 url parms에 담도록 변경

--- account/api/kakao_oauth_views.py
+++ account/api/kakao_oauth_views.py
@@ -46,10 +46,8 @@ class KakaoOauthViewSet(APIView):
         if self._is_user_exists(user_info["id"]):
             auth_token = self._get_auth_token(user_info["id"])
 
-            response = redirect(f"{FRONT_END_ENDPOINT}/login")
-            response.set_cookie(
-                "access_token", auth_token, max_age=1000, samesite="None", secure=True
-            )  # HTTPS에서만 쿠키 전송
+            response = redirect(f"{FRONT_END_ENDPOINT}/login?access_token={auth_token}")
+            response.set_cookie("access_token", auth_token, max_age=1000)
             return response
         return redirect(
             f'{FRONT_END_ENDPOINT}/signup?kakao_id={user_info["id"]}&username={user_info["properties"]["nickname"]}&profile_image=${user_info["properties"]["profile_image"]}&'
