21e2d4fbde44c4428279a267014c7f8b7016ee85
myoungjinGo-FE <cokemania2099@gmail.com>
2024-11-03 23:51:06 +0900

    🚧 Fix: AccessToken Lifetime 수정

--- forcatProject/settings.py
+++ forcatProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -123,6 +124,10 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=720),
+}
 
 
 # Internationalization
